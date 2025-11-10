f0635aef1e2289aca914729831852fcd9d0e824b
default <rabira.motuma@stu.ocsb.ca>
2025-11-10 19:45:29 +0000

    added new main.ts file for practicing const and variable uses

---
+++ main.ts
@@ -0,0 +1,25 @@
+/**
+ * author Rabira Motuma
+ * version 1.0.0
+ * date 2025-11-10
+ * fileoverview This program displays 3 constants and their sum.
+ */
+
+// retrieve numbers
+const NUMBER1: number = 10;
+const NUMBER2: number = -20;
+const NUMBER3: number = 85;
+
+// input - none
+
+// process
+// calculate the sum
+let answer: number = NUMBER1 + NUMBER2 + NUMBER3;
+
+// output
+// displays result
+console.log(
+  "The sum of " + NUMBER1 + ", " + NUMBER2 + " and " + NUMBER3 + " is: " + answer + "."
+);
+
+console.log("\.nDone");
